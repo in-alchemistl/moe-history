594f49e83f5bb891abf016a92069767501da5a5b
fimars <foru.fy@gmail.com>
2022-06-02 19:04:13 +0800

    init

---
+++ src/options/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>查看浏览历史</h1>
+    <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "App",
+})
+</script>
+
+<style>
+#app {
+    padding: 40px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
